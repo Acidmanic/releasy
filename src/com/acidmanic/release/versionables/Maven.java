7ecb2921246a8393cd00f8312e7567a8b25715db
Mani <goj_loj@yahoo.com>
2018-12-01 04:00:12 +0330

    Confirm Maven with XmlSpecFiledVersionable

--- src/com/acidmanic/release/versionables/Maven.java
+++ src/com/acidmanic/release/versionables/Maven.java
@@ -16,96 +16,41 @@
  */
 package com.acidmanic.release.versionables;
 
-import com.acidmanic.release.fileeditors.xmlinplace.XmlInPlaceEditor;
-import com.acidmanic.release.logging.Logger;
+import com.acidmanic.parse.stringcomparison.StringComparison;
 import com.acidmanic.release.versions.ReleaseTypes;
 import com.acidmanic.release.versions.Version;
-import com.acidmanic.utilities.FileSearch;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
  */
-public class Maven implements Versionable {
+public class Maven extends XmlSpecFiledVersionable {
 
     private static final String MAVEN_SNAPSHOT_POSTFIX = "-SNAPSHOT";
+    private static final String MAVEN_SPEC_FILE = "pom.xml";
+    private static final String[] VERSION_ADDRESS = {"project", "version"};
+    private static final int COMPARISON = StringComparison.COMPARE_CASE_SENSITIVE;
 
-    private boolean mavenPomPresent;
-    private File mavenPomFile;
-    private int releaseType;
-
-    @Override
-    public void setup(File directory, int releaseType) {
-        this.mavenPomFile = null;
-        this.mavenPomPresent = false;
-        File pom = new FileSearch().search(directory,"pom.xml");
-        if (pom != null && pom.exists()) {
-            this.mavenPomFile = pom;
-            this.mavenPomPresent = true;
-        }
-        this.releaseType = releaseType;
+    public Maven() {
+        super(MAVEN_SPEC_FILE, VERSION_ADDRESS, COMPARISON);
     }
 
     @Override
-    public boolean setVersion(Version version) {
-        if (mavenPomPresent) {
-            try {
-                setMavenProjectVersion(version, this.releaseType);
-                return true;
-            } catch (Exception e) {
-                Logger.log("Unable to set Version: " + e.getClass().getSimpleName(), this);
-            }
-        }
-        return false;
-    }
-
-    private void setMavenProjectVersion(Version version, int releaseType) throws IOException {
-        String mavenFileContent = new String(Files.readAllBytes(this.mavenPomFile.toPath()));
-        XmlInPlaceEditor editor = new XmlInPlaceEditor();
-        mavenFileContent = editor.setTagContent(new String[]{"project", "version"}, mavenFileContent,
-                mavenVersionStringFor(version, releaseType));
-        if (this.mavenPomFile.exists()) {
-            this.mavenPomFile.delete();
-        }
-        Files.write(this.mavenPomFile.toPath(), mavenFileContent.getBytes(), StandardOpenOption.CREATE);
-        Logger.log("Maven Project Version set.", this);
-    }
-
-    @Override
-    public boolean isPresent() {
-        return mavenPomPresent;
-    }
-
-    @Override
-    public List<String> getVersions() {
-        ArrayList<String> ret = new ArrayList<>();
-        try {
-            String mavenFileContent = new String(Files.readAllBytes(this.mavenPomFile.toPath()));
-            XmlInPlaceEditor editor = new XmlInPlaceEditor();
-            String mavenVersion = editor.getTagContent(new String[]{"project", "version"}, mavenFileContent);
-            if (mavenVersion.endsWith(MAVEN_SNAPSHOT_POSTFIX)) {
-                mavenVersion = mavenVersion.substring(0,
-                        mavenVersion.lastIndexOf(MAVEN_SNAPSHOT_POSTFIX));
-            }
-            ret.add(mavenVersion);
-            return ret;
-        } catch (Exception e) {
-        }
-        return ret;
-    }
-
-    private String mavenVersionStringFor(Version version, int releaseType) {
+    protected String getVersionString(Version version, int releaseType) {
         String ret = version.getVersionString();
         if (releaseType != ReleaseTypes.STABLE) {
             ret += MAVEN_SNAPSHOT_POSTFIX;
         }
         return ret;
+    }
+
+    @Override
+    protected String procesVersionStringBeforeDeliver(String version) {
+        if (version.endsWith(MAVEN_SNAPSHOT_POSTFIX)) {
+            version = version.substring(0,
+                    version.lastIndexOf(MAVEN_SNAPSHOT_POSTFIX));
+        }
+        return version;
     }
 
 }
