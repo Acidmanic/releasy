974e6343d60cb5e87defe5326beaeea7a2a909c9
Mani <goj_loj@yahoo.com>
2018-12-03 21:08:34 +0330

    AddJGitFacade source control

---
+++ src/com/acidmanic/release/sourcecontrols/JGitFacadeSourceControl.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018 Mani Moayedi (acidmanic.moayedi@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.acidmanic.release.sourcecontrols;
+
+import java.io.File;
+import org.eclipse.jgit.api.Git;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public class JGitFacadeSourceControl implements SourceControlSystem {
+
+    @Override
+    public void acceptLocalChanges(File directory, String description) {
+        Git git = tryGetGit(directory);
+        if (git != null) {
+            try {
+                git.add().call();
+                git.commit().setMessage(description).call();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    @Override
+    public boolean isPresent(File directory) {
+        Git git = tryGetGit(directory);
+        if(git!=null){
+            try {
+                git.status().call();
+                return true;
+            } catch (Exception e) {
+            }
+        }
+        return false;
+    }
+
+    private Git tryGetGit(File directory) {
+        try {
+            return Git.open(directory);
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+}
