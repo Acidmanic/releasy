857abd428493f47969662713a7b0d935f725805e
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2020-05-03 21:59:54 +0430

    Add DirectoryHelper

---
+++ src/com/acidmanic/utilities/DirectoryHelper.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2020 Acidmanic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.acidmanic.utilities;
+
+import com.acidmanic.release.versions.standard.VersionStandard;
+import java.io.File;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ *
+ * @author Acidmanic
+ */
+public class DirectoryHelper {
+
+    public void scan(File directory, Function<File, Boolean> validator, Consumer<File> scanner) {
+        
+        File[] files = directory.listFiles();
+
+        for (File file : files) {
+
+            if (validator.apply(file)) {
+
+                scanner.accept(file);
+            }
+        }
+    }
+    
+    public void scanFiles(File directory,Function<File, Boolean> validator, Consumer<File> scanner){
+        
+        scan(directory, f -> !f.isDirectory() && validator.apply(f),scanner);
+    }
+    
+    public void scanDirectories(File directory,Consumer<File> scanner){
+        
+        scan(directory, f -> f.isDirectory() ,scanner);
+    }
+    
+    public void scanTreeFiles(File directory, Function<File, Boolean> validator, Consumer<File> scanner){
+        
+        scanFiles(directory, validator, scanner);
+        
+        scanDirectories(directory, d -> scanTreeFiles(d, validator, scanner));
+    }
+}
