6c31674be4fdc9c26dc3adb410061b88b8bc6005
Mani Moayedi <you@example.com>
2021-04-10 08:47:46 +0430

    Refactor IncrementInputAnalyzer's package

--- src/com/acidmanic/release/commands/Auto.java
+++ src/com/acidmanic/release/commands/Auto.java
@@ -16,6 +16,7 @@
  */
 package com.acidmanic.release.commands;
 
+import com.acidmanic.release.commands.arguments.IncrementInputAnalyzer;
 import com.acidmanic.commandline.commands.TypeRegistery;
 import com.acidmanic.release.Releaser2;
 import com.acidmanic.release.directoryscanning.ReleaseWorkspace;
--- src/com/acidmanic/release/commands/arguments/IncrementInputAnalyzer.java
+++ src/com/acidmanic/release/commands/arguments/IncrementInputAnalyzer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.acidmanic.release.commands;
+package com.acidmanic.release.commands.arguments;
 
 import com.acidmanic.release.versions.standard.VersionSection;
 import com.acidmanic.release.versions.standard.VersionStandard;
